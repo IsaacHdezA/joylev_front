e4c12224de855084841768524793897026a2d851
IsaacHdezA <aldoisaachernandez@outlook.com>
2024-05-26 17:40:06 -0600

    [PROJECT]: Added Angular UI Material and TailwindCSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// Font Awesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAt, faKey, faUnlock } from '@fortawesome/free-solid-svg-icons';
+
+// Material UI
+import { MatCardModule } from '@angular/material/card';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FontAwesomeModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'joylev-records';
+  atIcon = faAt;
+  keyIcon = faKey;
+  unlockIcon = faUnlock;
+
 }
