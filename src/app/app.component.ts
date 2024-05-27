4af8e058400ebdbfb6e22f8bc144d44aa3e1fa6f
IsaacHdezA <aldoisaachernandez@outlook.com>
2024-05-26 20:44:05 -0600

    [LOGIN]: Added styles for login and register forms. This is a checkpoint before changing UI library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-// Font Awesome
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faAt, faKey, faUnlock } from '@fortawesome/free-solid-svg-icons';
-
-// Material UI
-import { MatCardModule } from '@angular/material/card';
-
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { AppLoginComponent } from '../views/app-login/app-login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MatCardModule,
-    MatIconModule,
-    MatInputModule,
-    MatFormFieldModule,
-    FontAwesomeModule,
+    AppLoginComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
-export class AppComponent {
-  atIcon = faAt;
-  keyIcon = faKey;
-  unlockIcon = faUnlock;
-
-}
+export class AppComponent {}
